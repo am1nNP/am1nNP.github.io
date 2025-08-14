439842a8df694c7da7493b4760f5276e68344a58
am1nNP <amin79nadipoor@gmail.com>
2025-08-14 16:31:19 +0330

    change humberger menu, sun and moon svg

--- app.js
+++ app.js
@@ -1,27 +1,31 @@
-const humb= document.querySelector("#humburger")
-const menu= document.querySelector("#menu")
-const moon= document.querySelector("#moon")
-const moon1= document.querySelector("#moon1")
-const hLinks= document.querySelectorAll("#hLink")
-humb.addEventListener("click",()=>{
-    menu.classList.toggle("hidden")
-    humb.classList.toggle("bg-white")
-})
+import { moonSVG, sunSVG } from './svgConfig.js';
+window.m = m;
+const humb = document.querySelector('#humburger');
+const menu = document.querySelector('#menu');
+const moon = document.querySelector('#moon');
+const moon1 = document.querySelector('#moon1');
+const hLinks = document.querySelectorAll('#hLink');
+humb.addEventListener('click', () => {
+  menu.classList.toggle('hidden');
+});
 
-hLinks.forEach(link=>{
-    link.addEventListener("click",()=>{
-        menu.classList.toggle("hidden")
-        humb.classList.toggle("bg-white")
-    })
-})
+hLinks.forEach((link) => {
+  link.addEventListener('click', () => {
+    menu.classList.toggle('hidden');
+  });
+});
 
-moon.addEventListener("click",()=>{
-    moon1.classList.toggle("dark")
-})
-function m(id){
-    let obj = document.getElementById(id);
-    let src=obj.src;
-    window.open(src,'Image','width=largeImage.stylewidth,height=largeImage.style.height,resizable=1')
+moon.addEventListener('click', () => {
+  moon1.classList.toggle('dark');
+  let res = moon1.classList.contains('dark');
+  res ? (moon.innerHTML = sunSVG) : (moon.innerHTML = moonSVG);
+});
+function m(id) {
+  let obj = document.getElementById(id);
+  let src = obj.src;
+  window.open(
+    src,
+    'Image',
+    'width=largeImage.stylewidth,height=largeImage.style.height,resizable=1'
+  );
 }
-
-
